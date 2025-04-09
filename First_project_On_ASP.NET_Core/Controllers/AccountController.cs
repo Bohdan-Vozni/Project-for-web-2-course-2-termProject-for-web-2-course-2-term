4d257061aa3ff2bf137821b69ab12e37d5da0241
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-09 18:23:43 +0300

    Add logic login in site without personall account
    created a controller and site template, login logic to the site without
    a personal account

---
+++ First_project_On_ASP.NET_Core/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Data.Models;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Shop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDBContent content;
+        public AccountController (AppDBContent content)
+        {
+            this.content = content;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = content.User.FirstOrDefault(u => u.login == model.login && u.password == model.password);
+
+                if (user != null)
+                {
+                    HttpContext.Session.SetString("User", user.name);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Error with login or password");
+
+            }
+            return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("User");
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
