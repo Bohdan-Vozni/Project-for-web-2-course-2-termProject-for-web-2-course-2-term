19e9a68c087e82deaa7b32ffb0b8a973d6b1757c
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-14 00:14:29 +0300

    Add form find=> change auto and conect to style
    added a template for searching for a car before changing it and added
    styles to it (search does not work in search), the form for changing and
    adding a car is done but the logic has not been added yet

--- First_project_On_ASP.NET_Core/Controllers/AdminZoneController.cs
+++ First_project_On_ASP.NET_Core/Controllers/AdminZoneController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
 using Shop.Data.interfaces;
 using Shop.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Shop.Controllers
 {
     public class AdminZoneController : Controller
     {
         private readonly ICarsCategory _allCategories;
+        private readonly IAllCars _allCars;
+        private AppDBContent content;
 
+        public AdminZoneController (AppDBContent content)
+        {
+            this.content = content;
+        }
         public IActionResult AdminHome()
         {
             return View();
@@ -17,6 +27,35 @@ namespace Shop.Controllers
         public IActionResult AddAuto()
         {
             //ViewBag.Categories = _allCategories.AllCegories;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FindAutoForChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FindAutoForChange(Car car)
+        {
+           List<Car> allFindCar = content.Car.Where
+                (
+                 c =>
+                 c.name == car.name ||
+                 c.price == car.price ||
+                 c.isFavourite == car.isFavourite ||
+                 c.available == car.available ||
+                 c.category == car.category
+                ).ToList();
+
+            return View(allFindCar);  //використати ViewModels
+        }
+
+
+        [HttpGet]
+        public IActionResult ChangeInfoAuto()
+        {
             return View(new Car());
         }
     }
