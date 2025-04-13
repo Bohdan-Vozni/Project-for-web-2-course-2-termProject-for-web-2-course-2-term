88ea6d186dcfca1da127e163fdfa095ad5e45c14
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-13 23:06:07 +0300

    Add Admin Zone
    Added admin zone, changed layout template, added admin zone controller,
    added template and also added car template

---
+++ First_project_On_ASP.NET_Core/Controllers/AdminZoneController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.interfaces;
+using Shop.Data.Models;
+
+namespace Shop.Controllers
+{
+    public class AdminZoneController : Controller
+    {
+        private readonly ICarsCategory _allCategories;
+
+        public IActionResult AdminHome()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddAuto()
+        {
+            //ViewBag.Categories = _allCategories.AllCegories;
+            return View(new Car());
+        }
+    }
+}
