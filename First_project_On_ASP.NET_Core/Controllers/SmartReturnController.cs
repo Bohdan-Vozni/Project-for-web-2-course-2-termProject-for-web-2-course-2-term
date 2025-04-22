3421d1f7fb6d54b672c3ca3604dfc685de1e2e1f
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-22 19:01:57 +0300

    Add smart return controller and template

---
+++ First_project_On_ASP.NET_Core/Controllers/SmartReturnController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Shop.Controllers
+{
+    public class SmartReturnController : Controller
+    {
+
+        public IActionResult ListFunc()
+        {
+            return View();
+        }
+
+    }
+}
