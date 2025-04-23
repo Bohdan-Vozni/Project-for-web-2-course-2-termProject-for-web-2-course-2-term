5ef5c4e9626a78d06044471d013dcbc5ff86605a
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-24 00:25:56 +0300

    Add logic History renting cars in personal account

--- First_project_On_ASP.NET_Core/Controllers/DashboardController.cs
+++ First_project_On_ASP.NET_Core/Controllers/DashboardController.cs
@@ -5,17 +5,18 @@ using Microsoft.EntityFrameworkCore;
 using Shop.Data;
 using Shop.Data.Models;
 using Shop.ViewModels;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text.Json;
 
 namespace Shop.Controllers
 {
-    public class DashboardController  : Controller
+    public class DashboardController : Controller
     {
         private readonly AppDBContent content;
 
-        public DashboardController(AppDBContent content) 
+        public DashboardController(AppDBContent content)
         {
             this.content = content;
         }
@@ -23,7 +24,7 @@ namespace Shop.Controllers
         public IActionResult Personal_Account()
         {
             var getUserInfo = HttpContext.Session.GetString("User");
-            var jsonDeserializeUser = JsonSerializer.Deserialize<User>(getUserInfo);            
+            var jsonDeserializeUser = JsonSerializer.Deserialize<User>(getUserInfo);
 
             return View(jsonDeserializeUser);
         }
@@ -40,7 +41,7 @@ namespace Shop.Controllers
         [HttpPost]
         public IActionResult EditPersonalData(User user, int id)
         {
-            var findUser =content.User.FirstOrDefault(u => u.id == id);
+            var findUser = content.User.FirstOrDefault(u => u.id == id);
 
             findUser.name = user.name;
             findUser.surname = user.surname;
@@ -61,6 +62,40 @@ namespace Shop.Controllers
             HttpContext.Session.SetString("User", serializationUser);
 
             return View("Personal_Account", findUpdateUser);
+        }
+
+        [HttpGet]
+        public IActionResult HistoryRent()
+        {
+
+            var user = HttpContext.Session.GetString("User");
+            if (string.IsNullOrEmpty(user))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var userDeserialize = JsonSerializer.Deserialize<User>(user);
+
+            // Отримуємо повернені замовлення з підвантаженням всіх необхідних даних
+            var returnedOrderIds = content.OrderDetailReturn
+                .Where(c => c.isReturning == true)
+                .Include(c => c.orderDetail)
+                    .ThenInclude(od => od.car) // Додаємо підвантаження автомобіля
+                .Include(c => c.orderDetail)
+                    .ThenInclude(od => od.user) // Додаємо підвантаження користувача
+                .Include(c => c.placeReturn)
+                .ToList();
+
+            var result = returnedOrderIds.Where(c => c.orderDetail.user.id == userDeserialize.id).ToList();
+
+
+            // Створюємо модель і гарантуємо, що список не буде null
+            var modelReturnAuto = new SmartReturnAutoForCustomerViewModel
+            {
+                userNotActiveOrders = result ?? new List<OrderDetailReturn>() // Забезпечуємо, що список не null
+            };
+
+            return View(modelReturnAuto);
         }
     }
 }
