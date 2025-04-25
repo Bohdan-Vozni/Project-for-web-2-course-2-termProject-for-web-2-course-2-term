2c4faa42fb95ef31d37f08a0184923e0d9fadf70
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-25 11:46:51 +0300

    Add layout for review and controller

---
+++ First_project_On_ASP.NET_Core/Controllers/ReviewController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ReviewController : Controller
+    {
+        [HttpGet]
+        public IActionResult ReviewForSite()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ReviewForSite(int s) // добавив int s щоб не було полки
+        {
+            return View();
+        }
+    }
+}
