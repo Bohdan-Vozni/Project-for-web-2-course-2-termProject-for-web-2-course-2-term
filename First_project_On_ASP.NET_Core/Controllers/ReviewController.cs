a0905e11a787f9331a339c812c0aa094f0a68215
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-04-28 19:17:43 +0300

    Add logic for add reviews

--- First_project_On_ASP.NET_Core/Controllers/ReviewController.cs
+++ First_project_On_ASP.NET_Core/Controllers/ReviewController.cs
@@ -1,19 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Data.Models;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 
 namespace Shop.Controllers
 {
     public class ReviewController : Controller
     {
+        private readonly AppDBContent content;
+        public ReviewController(AppDBContent content)
+        {
+            this.content = content;
+        }
+
         [HttpGet]
         public IActionResult ReviewForSite()
         {
-            return View();
+            List<Reviews> listReviews = content.Reviews.Where(c => c.name == c.name).OrderByDescending(c => c.timeSend).ToList();
+
+            var model = new ReviewForSiteViewModel()
+            {
+                listReviews = listReviews
+            };
+
+            return View(model);
         }
 
+
         [HttpPost]
-        public IActionResult ReviewForSite(int s) // добавив int s щоб не було полки
+        public IActionResult ReviewForSite(ReviewForSiteViewModel model) 
         {
-            return View();
+            var user = HttpContext.Session.GetString("User");
+
+            if (user == null)
+            {
+                ModelState.AddModelError("Review.name", "Ви не авторизовані потрібно авторизуватися");
+
+                return View();
+            }
+
+            var userDeSerialise = JsonSerializer.Deserialize<User>(user);
+
+            var ReviewsForAdd = new Reviews()
+            {
+                userId = userDeSerialise.id,
+                name = model.Review.name,
+                response = model.Review.response,
+                timeSend = DateTime.Now,    
+            };
+
+            content.Reviews.Add(ReviewsForAdd);
+            content.SaveChanges();
+
+            List<Reviews> listReviews = content.Reviews
+                .OrderByDescending(c => c.timeSend) 
+                .ToList();
+
+            var modelForRetutn = new ReviewForSiteViewModel()
+            {
+                listReviews = listReviews
+            };
+
+            return View(modelForRetutn);
         }
     }
 }
---
+++ First_project_On_ASP.NET_Core/ViewModels/ReviewForSiteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop.ViewModels
+{
+    public class ReviewForSiteViewModel
+    {
+        public Reviews Review { get; set; }
+
+        public List<Reviews> listReviews { get; set; }
+    }
+}
