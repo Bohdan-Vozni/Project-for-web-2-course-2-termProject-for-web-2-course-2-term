24412eaa86318afc1dd2fbfa371c6d685f49ff51
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-05-03 19:04:44 +0300

    Fix problem with retutn to admin_home and listfunc
    when we added auto we had a problem because we later delete template

--- First_project_On_ASP.NET_Core/Controllers/AdminZoneController.cs
+++ First_project_On_ASP.NET_Core/Controllers/AdminZoneController.cs
@@ -31,7 +31,7 @@ namespace Shop.Controllers
             _allCars = allCars;
             _allPlace = allPlace;
         }
-        public IActionResult AdminHome()
+        public IActionResult Admins()
         {
             return View();
         }
@@ -81,7 +81,7 @@ namespace Shop.Controllers
             content.Car.Add(car);
             content.SaveChanges();
 
-            return RedirectToAction("AdminHome");
+            return RedirectToAction("Index","Home");
         }
 
         [HttpGet]
@@ -238,7 +238,7 @@ namespace Shop.Controllers
             content.Car.Update(carForUpdate);
             content.SaveChanges();
 
-            return RedirectToAction("AdminHome");
+            return RedirectToAction("Index","Home");
         }
 
 
@@ -390,7 +390,7 @@ namespace Shop.Controllers
 
             if (string.IsNullOrEmpty(user))
             {
-                return RedirectToAction("AdminHome", "Account"); 
+                return RedirectToAction("Index", "Home"); 
             }
             var userDeserialize = JsonSerializer.Deserialize<User>(user);
 
